14226cebccca0cf9d97c5b2bce620f599e4da645
mohammad st <mohammad.sayartehrani@yahoo.com>
2021-03-08 03:17:32 +0330

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      HomeComponent,
+      ProjectsComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
